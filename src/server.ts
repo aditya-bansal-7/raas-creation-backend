be680d18fa6d9357859792c4668a1f97f8c976c5
Aditya Bansal <adityabansalsafe@gmail.com>
2025-05-30 14:33:37 +0530

    add console which route hit

--- src/server.ts
+++ src/server.ts
@@ -28,6 +28,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+  next();
+});
+
 // Show routes called in console during development
 if (ENV.NODE_ENV === NodeEnvs.Dev) {
   app.use(morgan('dev'));
